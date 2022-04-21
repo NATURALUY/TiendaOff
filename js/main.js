d7f937424a41e088ede882974f3eea871d78068e
Alvaro <alvamoreno@gmail.com>
2022-04-21 19:46:36 -0300

    Seccion damas se configuro el evento click al boton para realizar la compra , item anillos

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@ let stock = ["campera", "buzo", "pollera", "reloj" ,"anillo","gorro","sobretodo"
 //clase articulo
 
 class Articulo {
-    constructor(nombre, tipo, talle, precio) {
-      this.nombre = nombre;
-      this.tipo = tipo;
+    constructor(nombre, talle, precio) {
+      this.nombre = nombre;     
       this.talle = talle;
       this.precio = precio;      
     }
@@ -26,121 +25,121 @@ class Articulo {
 
 let venta =[];
 
-verStock= ("Desea ver el listado de productos disponibles : SI / NO ")
+// verStock= ("Desea ver el listado de productos disponibles : SI / NO ")
 
-const venderArticulo =()=> {    
+// const venderArticulo =()=> {    
 
-    let nombreArticulo = prompt ("Ingrese el nombre del articulo que desea comprar :?");
-    nombreArticulo = nombreArticulo.toLowerCase();
-    while (!stock.includes(nombreArticulo)){    
-    alert("No Se encuentra el producto en stock");    
-    nombreArticulo = prompt ("Ingrese el nombre del articulo que desea comprar :?");
-    nombreArticulo = nombreArticulo.toLowerCase();
-    }
-    let TipoArticulo = prompt(" Tipo DAMA / CABALLERO : ? ");    
-    TipoArticulo = TipoArticulo.toUpperCase();
-    while ((TipoArticulo !== "DAMA") && (TipoArticulo!== "CABALLERO")){
-      TipoArticulo = prompt("Usted ingreso un tipo incorrecto!!,  solo acepta las opciones DAMA / CABALLERO : ");   
-      TipoArticulo = TipoArticulo.toUpperCase();
-    } 
-    let talleArticulo= prompt("Ingrese el talle del articulo : ?");
-    talleArticulo = talleArticulo.toLocaleUpperCase();
-    while ((talleArticulo !== "S") && (talleArticulo !== "M") && (talleArticulo !=="L")) {
-      talleArticulo = prompt("Usted ingreso un talle incorrecto!!,  solo acepta S-M-L Ingrese el talle del articulo : ");    
-      talleArticulo = talleArticulo.toUpperCase();
-    }    
-    let precioArticulo= parseInt(prompt(`Ingrese el precio del /la  ${nombreArticulo}  $ :`));  
-    while (!Number.isInteger(precioArticulo)){
-      precioArticulo= parseInt(prompt(`El monto ingresado incorrecto !!! Ingrese el monto del producto :  ${nombreArticulo}  $ :`));  
-    }
-    const articulo1 = new Articulo (nombreArticulo,TipoArticulo,talleArticulo, precioArticulo); 
+//     let nombreArticulo = prompt ("Ingrese el nombre del articulo que desea comprar :?");
+//     nombreArticulo = nombreArticulo.toLowerCase();
+//     while (!stock.includes(nombreArticulo)){    
+//     alert("No Se encuentra el producto en stock");    
+//     nombreArticulo = prompt ("Ingrese el nombre del articulo que desea comprar :?");
+//     nombreArticulo = nombreArticulo.toLowerCase();
+//     }
+//     let TipoArticulo = prompt(" Tipo DAMA / CABALLERO : ? ");    
+//     TipoArticulo = TipoArticulo.toUpperCase();
+//     while ((TipoArticulo !== "DAMA") && (TipoArticulo!== "CABALLERO")){
+//       TipoArticulo = prompt("Usted ingreso un tipo incorrecto!!,  solo acepta las opciones DAMA / CABALLERO : ");   
+//       TipoArticulo = TipoArticulo.toUpperCase();
+//     } 
+//     let talleArticulo= prompt("Ingrese el talle del articulo : ?");
+//     talleArticulo = talleArticulo.toLocaleUpperCase();
+//     while ((talleArticulo !== "S") && (talleArticulo !== "M") && (talleArticulo !=="L")) {
+//       talleArticulo = prompt("Usted ingreso un talle incorrecto!!,  solo acepta S-M-L Ingrese el talle del articulo : ");    
+//       talleArticulo = talleArticulo.toUpperCase();
+//     }    
+//     let precioArticulo= parseInt(prompt(`Ingrese el precio del /la  ${nombreArticulo}  $ :`));  
+//     while (!Number.isInteger(precioArticulo)){
+//       precioArticulo= parseInt(prompt(`El monto ingresado incorrecto !!! Ingrese el monto del producto :  ${nombreArticulo}  $ :`));  
+//     }
+//     const articulo1 = new Articulo (nombreArticulo,TipoArticulo,talleArticulo, precioArticulo); 
     
-    return (articulo1);
-}
-let contador =0;
-let precioTotal=0;
+//     return (articulo1);
+// }
+// let contador =0;
+// let precioTotal=0;
 
-function listarCompras(){
-if (venta.length == 0){
-  alert("Usted aun no realizo ninguna compra  cantidad de items. " + venta.length);
-}else {
-  console.log("Resumen de venta -- TIENDA OFF !!!");
-  for (i = 0; i < venta.length; i ++){
-    console.log("Item " + i + " - " + " Articulo " + venta[i].nombre + " Talle - " + venta[i].talle + " Precio $U " + venta[i].precio);
-  }
-  alert("Precio total $ " + precioTotal + " de un total de " + contador + " compras realizadas");      
-}
-}
+// function listarCompras(){
+// if (venta.length == 0){
+//   alert("Usted aun no realizo ninguna compra  cantidad de items. " + venta.length);
+// }else {
+//   console.log("Resumen de venta -- TIENDA OFF !!!");
+//   for (i = 0; i < venta.length; i ++){
+//     console.log("Item " + i + " - " + " Articulo " + venta[i].nombre + " Talle - " + venta[i].talle + " Precio $U " + venta[i].precio);
+//   }
+//   alert("Precio total $ " + precioTotal + " de un total de " + contador + " compras realizadas");      
+// }
+// }
 
-// Programa ....
-menu()
-opcion = parseInt(prompt("Ingrese la opcion que desea realizar : "));
-while ((opcion < 6 ) && (opcion > 1)){   
-  switch (opcion){
-    case 1: 
-      stock.sort();
-      console.log("Listado de productos ordenados alfabeticamente")
-      for (let i =0 ; i < stock.length ; i++){
-        console.log("item -> " + i + " - " + stock[i]);
-      }
-      break;
-    case 2:    
-      let art = venderArticulo();
-      let precio = art.calcularPrecio();
-      alert("Se vendió el/la " + art.nombre + " al costo : $" + precio); 
-      venta.push(art);
-      contador = contador +1;
-      precioTotal= precioTotal + precio;
-      let continuar = prompt("Desea continuar realizando compras ?  SI para continuar / Otra tecla para Finalizar");
-      continuar=continuar.toUpperCase();
+// // Programa ....
+// menu()
+// opcion = parseInt(prompt("Ingrese la opcion que desea realizar : "));
+// while ((opcion < 6 ) && (opcion > 1)){   
+//   switch (opcion){
+//     case 1: 
+//       stock.sort();
+//       console.log("Listado de productos ordenados alfabeticamente")
+//       for (let i =0 ; i < stock.length ; i++){
+//         console.log("item -> " + i + " - " + stock[i]);
+//       }
+//       break;
+//     case 2:    
+//       let art = venderArticulo();
+//       let precio = art.calcularPrecio();
+//       alert("Se vendió el/la " + art.nombre + " al costo : $" + precio); 
+//       venta.push(art);
+//       contador = contador +1;
+//       precioTotal= precioTotal + precio;
+//       let continuar = prompt("Desea continuar realizando compras ?  SI para continuar / Otra tecla para Finalizar");
+//       continuar=continuar.toUpperCase();
       
-      while (continuar == "SI"){
-        art = venderArticulo();
-        precio = art.calcularPrecio();  
-        alert("Se vendió el/la " + art.nombre + " al costo : $" + precio); 
-        venta.push(art);
-        contador = contador +1;
-        precioTotal= precioTotal + precio;
-        continuar = prompt("Desea continuar realizando compras ?  SI para continuar / Otra tecla para Finalizar "); 
-        continuar= continuar.toUpperCase(); 
-      }
-      break;  
-    case 3:
-      listarCompras();
-      break;
-    case 4: 
-      let buscar = prompt("Ingrese el producto que desea buscar en stock:");
-      const buscarProducto = stock.find(el => el == buscar);
-      console.log(buscarProducto);
-      if (buscarProducto == buscar){
-        alert(`el siguiente producto ${buscarProducto} se encuentra en stock`);
+//       while (continuar == "SI"){
+//         art = venderArticulo();
+//         precio = art.calcularPrecio();  
+//         alert("Se vendió el/la " + art.nombre + " al costo : $" + precio); 
+//         venta.push(art);
+//         contador = contador +1;
+//         precioTotal= precioTotal + precio;
+//         continuar = prompt("Desea continuar realizando compras ?  SI para continuar / Otra tecla para Finalizar "); 
+//         continuar= continuar.toUpperCase(); 
+//       }
+//       break;  
+//     case 3:
+//       listarCompras();
+//       break;
+//     case 4: 
+//       let buscar = prompt("Ingrese el producto que desea buscar en stock:");
+//       const buscarProducto = stock.find(el => el == buscar);
+//       console.log(buscarProducto);
+//       if (buscarProducto == buscar){
+//         alert(`el siguiente producto ${buscarProducto} se encuentra en stock`);
 
-      }else{
-        alert(`el siguiente producto NO se encuentra en stock`);
-      }
-      break;  
-      case 5: 
-      let cantidadArticulos = venta.length;
-      let eliminar = prompt("Ingrese el producto que desea buscar para eliminar del carrito: ?");
-      const eliminarProducto = venta.findIndex(element => element.nombre === eliminar);
-      if (eliminarProducto !== -1 ){
-        seguir = confirm("Realmente desea borrar el producto seleccionado ? ");
-        precioTotal = precioTotal - venta[eliminarProducto].precio;
-        venta.splice(eliminarProducto, 1);
-        if (venta.length < cantidadArticulos){
-          alert(`Producto ${eliminar} eliminado con exito.....`);
-          contador = contador -1;
-          listarCompras();
-        }  
-      }else {
-        alert("No existe el producto en el carrito")
-      }
-      break;  
-  }      
-  menu()
-  opcion = parseInt(prompt("Ingrese la opción :"))
-}
-alert("Usted finalizó sus compras");
+//       }else{
+//         alert(`el siguiente producto NO se encuentra en stock`);
+//       }
+//       break;  
+//       case 5: 
+//       let cantidadArticulos = venta.length;
+//       let eliminar = prompt("Ingrese el producto que desea buscar para eliminar del carrito: ?");
+//       const eliminarProducto = venta.findIndex(element => element.nombre === eliminar);
+//       if (eliminarProducto !== -1 ){
+//         seguir = confirm("Realmente desea borrar el producto seleccionado ? ");
+//         precioTotal = precioTotal - venta[eliminarProducto].precio;
+//         venta.splice(eliminarProducto, 1);
+//         if (venta.length < cantidadArticulos){
+//           alert(`Producto ${eliminar} eliminado con exito.....`);
+//           contador = contador -1;
+//           listarCompras();
+//         }  
+//       }else {
+//         alert("No existe el producto en el carrito")
+//       }
+//       break;  
+//   }      
+//   menu()
+//   opcion = parseInt(prompt("Ingrese la opción :"))
+// }
+// alert("Usted finalizó sus compras");
 
 
 // muestra stock / crea cards con foto de los elementos
@@ -204,15 +203,17 @@ botonBuscar.onclick =(e) => {
 // comprar  
 
 const btnComprar = document.getElementById("Comprar")
-
-
+venta = [];
 
 btnComprar.onclick = (e) => {
   let nombre = document.querySelector("#articulo").textContent;
-  let tipo = "DAMA";
   let talle = document.querySelector("#talle").value
-  let precio = document.querySelector("#precio").textContent
-  const articulo1 = new Articulo (nombre,tipo,talle, precio); 
-  console.log(nombre + tipo + talle + precio);  
+  let precio = document.querySelector("#precio").textContent  
+  const articulo1 = new Articulo (nombre,talle, precio);   
+  let precioFinal = articulo1.calcularPrecio();
+  console.log(precioFinal);  
+  console.log(articulo1.resumenVenta());
 }
+
+
 
